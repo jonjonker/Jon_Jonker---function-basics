874bbf393ef9e209130558045c150d43af6be23e
Jón Jonker <66784287+jonjonker@users.noreply.github.com>
2021-04-11 15:52:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const makePizza = function () {
+    console.log("Make dough")
+    console.log("Add sauce")
+    console.log("Add vegetables")
+    console.log("Add Salami")
+    console.log("Add Olives")
+    console.log("Don't add cheese")
+};
+
+
+const makeSushi = function () {
+    console.log("prepare sushi rice")
+    console.log("Put rice on seaweed ")
+    console.log("add salmon")
+    console.log("roll sushi")
+    console.log("cut roll into pieces")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
